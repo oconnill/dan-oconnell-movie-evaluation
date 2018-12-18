dde2adc2993f631f4a30e930e9b689b54da4b998
oconnill <doconnelldesign@gmail.com>
2018-12-18 10:14:58 -0700

    Created AJAX requests for full movie list and single movie list.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="container">
-      <div id="app">
-        <Component1></Component1>
-        </div>
+    <div id="app"><Component1></Component1></div>
   </div>
 </template>
 
 <script>
-  import Component1 from './components/Component1.vue'
-  import '../node_modules/bootstrap/scss/bootstrap.scss';
-  import '../public/style.scss';
+  import Component1 from "./components/Component1.vue";
+  import "../node_modules/bootstrap/scss/bootstrap.scss";
+  import "../public/style.scss";
 
   export default {
-    name: 'app',
+    name: "app",
     components: {
       Component1
     }
-  }
+  };
 </script>
--- store/store.js
+++ store/store.js
@@ -5,9 +5,47 @@ import axios from 'axios'
 vue.use(vuex);
 
 export const store = new vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+
+  state: {
+    fullMovieList = [],
+    singleMovieModal = [],
+    handleError = []
+  },
+
+  mutations: {
+    setFullMovieList(state, data) {
+state.fullMovieList = data;
+    },
+    setSingleMovie(state, data){
+state.singleMovieModal = data;
+    },
+    handleError(state, data){
+      state.handleError = data;
+          }  
+  },
+
+  actions: {
+getFullMovieList({commit, dispatch}) {
+axios.get('http://candidate-test.icapture.com/oconnelld/movies.php')
+.then(response => {
+  commit('setFullMovieList', response.data)
+})
+.catch(err => {
+  commit("handleError", err);
+});
+},
+getSingleMovie({commit, dispatch}, id) {
+  axios.get('http://candidate-test.icapture.com/oconnelld/movies.php?movieid=' + id)
+  .then(response => {
+    commit('setSingleMovie', response.data)
+  })
+  .catch(err => {
+    commit("handleError", err);
+  });
+},
+
+
+  }
 });
 
 export default store;
